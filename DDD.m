685b432a2f2e6d361bfce4345e06e06fca5695e4
Antonio <Antonio@10.13.128.195>
2020-03-09 21:35:05 -0700

    Able to recieve sharpened grayscale image of eye. Stored in var: sharp_crop.

--- DDD.m
+++ DDD.m
@@ -2,25 +2,22 @@
 
 load('test.mat')
 
-se = strel('disk', 2);
+% se1 = strel('disk', 2);
+% se2 = strel('disk',3);
+H1 = fspecial('average');
 for i = 1:length(files)
     figure(1)
     image = I.image{i};
-%     image = insertShape(image,'rectangle',[(LE(i,:) - [8,6]) 20 14], 'Color', 'green');
-%     image = insertShape(image,'rectangle',[(RE(i,:) - [10,6]) 20 14], 'Color', 'green');
-    crop1 = imcrop(image, [(RE(i,:) - [12,6]) 19 14]);
-    crop2 = imcrop(image, [(LE(i,:) - [10,6]) 19 14]);
-%     imshow(image)
-%     pause;
-    crop = [1.2*crop1 crop2];
-    crop = crop(:,:,2);
-    crop = imadjust(crop, [0.15 0.6], [],2);
-    subplot(1,2,1)
-    imshow(crop)
-    subplot(1,2,2)
-    binary = (crop>90) & (crop<150);
-    binary = imclose(binary, se);
-    binary = imopen(binary,se);
-    imshow(binary)
+    crop1 = imcrop(image, [(RE(i,:) - [13,7]) 22 13]);
+    crop2 = imcrop(image, [(LE(i,:) - [11,7]) 22 13]);
+    crop = [1.2*crop1(:,:,2) zeros(14, 5) 0.9*crop2(:,:,2)];
+    crop = imadjust(crop, [0.3 0.75], [],1.75);
+    crop = imfilter(crop, H1);
+%     subplot(1,2,1)
+%     imshow(crop)
+%     subplot(1,2,2)
+    sharp_crop = imsharpen(crop);
+    sharp_crop = imadjust(sharp_crop, [0.2, 0.6], []);
+    imshow(sharp_crop);
     pause;
 end 
