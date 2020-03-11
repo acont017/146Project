141283503747d66a2904a977ae9b9abcfbf60ed1
antonio <acont017@ucr.edu>
2020-03-11 01:38:13 -0700

    Small bug fixes for plotting avgerage.
    Signed-off-by: antonio <acont017@ucr.edu>

--- DDD.m
+++ DDD.m
@@ -1,5 +1,7 @@
 function DDD(I, Eyes, thresh)
     f = uifigure;
+    thresh_vect = thresh*ones(size(I));
+    avg = zeros(size(I));
     for i=1:length(Eyes)
         figure(1)
         image = I{i};
@@ -12,30 +14,33 @@ function DDD(I, Eyes, thresh)
             eyes = Eyes{i};
         end
         eyes = imadjust(eyes,stretchlim(eyes, [0.005 .99]),[]);
-    %     eyes = im2bw(eyes);
-        imshow(eyes);
+        imshow(eyes)
         subplot(2,2,4)
         imhist(eyes);
         subplot(2,2,3)
         [histVal, pixVal] = imhist(eyes);
         avg(i) = sum(histVal.*pixVal)/sum(histVal);
-        if (avg(i) > thresh)
-            if ((avg(i-2)>thresh) && (avg(i-1) > thresh))
-                if (~(ishandle(f)))
-                    f = uifigure;
-                    uialert(f,'PULL OVER and GET SLEEP','Invalid File');
-                    pause;
+        if(i > 3)
+            if (avg(i) > thresh)
+                if ((avg(i-2)>thresh) && (avg(i-1) > thresh))
+                    if (~(ishandle(f)))
+                        f = uifigure;
+                        uialert(f,'PULL OVER and GET SLEEP','Invalid File');
+                        pause;
+                    end
+                end
+            else
+                if(ishandle(f))
+                    delete(f)
                 end
             end
-        else
-            if(ishandle(f))
-                delete(f)
-            end
         end
-        i;
-        plot(avg)
         
-%         pause;
+        plot(avg(1:i))
+        hold on
+        plot(thresh_vect(1:i))
+        hold off
+        
     end
     pause;
 end
